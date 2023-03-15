548e9a7adc51994dc4fe24ddb8dc1d20ae8542da
Miri <mirialin.39@gmail.com>
2023-03-15 23:43:56 +0200

    Set up

---
+++ src/main/java/com/library/PergamumLibrary/PergamumLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.library.PergamumLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PergamumLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PergamumLibraryApplication.class, args);
+	}
+
+}
